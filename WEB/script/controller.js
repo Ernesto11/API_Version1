086b09ca37c58d974b0ffc6d22e82b0b3c7d5fd4
olmancp <olmancp95@gmail.com>
2018-07-02 22:48:57 -0600

    WEB
    >conexion angular
    >plantilla

---
+++ WEB/script/controller.js
@@ -0,0 +1,30 @@
+var app = angular.module("app",[]);
+app.controller("main_controller",function($scope,$http){
+    $scope.posts=[];
+    $scope.new_post = {};
+    $scope.productos ={};
+    $http.post("http://localhost:8686/ApiV1/rest/api/get_products", {
+
+    })
+    .success(function(data,status,headers,config){
+        $scope.productos=data;
+    })
+    .error(function(err,status,headers,config){
+        console.log(err);
+    });
+    
+    $scope.add_post = function(){  
+        $http.post("http://localhost:8686/ApiV1/rest/api/get_products", {
+            usuario:"java",
+            contrasena: "1234"
+        })
+
+        .success(function(data,status,headers,config){
+            $scope.usuario=data;
+        })
+        .error(function(err,status,headers,config){
+            console.log(err);
+        });
+    };
+    
+});
