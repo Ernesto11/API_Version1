5c1b5977ac3ea32f932c064b78cb8db84458df3f
olmancp <olmancp95@gmail.com>
2018-07-07 13:08:17 -0600

    API
    ->Configuracion

--- ApiV1/src/main/java/api/controlador/DAO/Dao_DB.java
+++ ApiV1/src/main/java/api/controlador/DAO/Dao_DB.java
@@ -3,6 +3,7 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import api.controlador.Configuracion;
 import api.modelo.Producto;
 
 public class Dao_DB {
@@ -12,8 +13,7 @@ public class Dao_DB {
 	public void conexion(){
 		try{		
 			Class.forName("com.mysql.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_erp","root","");
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3333/db_erp","root","");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:"+Configuracion.getConfiguracion().getPORT()+"/db_erp","root","");
 		}catch(Exception e){
 			
 		}
